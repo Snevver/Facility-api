69af74f8e529549bc4531717d86458bac13f8ba8
Snevver <svenhoeksema@hotmail.com>
2025-04-27 21:48:25 +0200

    Added back the test calls so the README is still accurate

--- routes/routes.php
+++ routes/routes.php
@@ -3,6 +3,8 @@
 /** @var Bramus\Router\Router $router */
 
 // Define routes here
+$router->get('/test', App\Controllers\IndexController::class . '@test');
+ $router->get('/', App\Controllers\IndexController::class . '@test');
 $router->get('/facility', App\Controllers\FacilityController::class . '@getAllFacilities');
 $router->get('/facility/{id}', App\Controllers\FacilityController::class . '@getFacility');
 $router->post('/create', App\Controllers\FacilityController::class . '@createFacility');
